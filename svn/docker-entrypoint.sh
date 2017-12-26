c79742d6dc76bc88bcb3ac0a4b2ca56dd557b603
Emanuele Disco <emawind84@gmail.com>
2017-12-26 14:27:49 +0900

    update

--- svn/docker-entrypoint.sh
+++ svn/docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2.pid
 
-if ! [ "$(ls -A /mnt/svn)" ]; then
+if ! [ "$(ls -A $SVNROOT)" ]; then
     echo "Creating repository [repos]..."
-    svnadmin create --fs-type fsfs /mnt/svn/repos
+    svnadmin create --fs-type fsfs $SVNROOT/repos
 fi
 
 if [ $# -eq 0 ]; then
